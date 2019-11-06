ce888f55105526d2665e7adf7a5ab8775a25d093
广学 杨 <guangxueyang@guangxuedeMacBook-Pro.local>
2019-11-06 19:42:47 +0800

    Change Hi to allow multilang

--- testmod.go
+++ testmod.go
@@ -1,8 +1,23 @@
 package testmod
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func  Hi(name string) string {
-	fmt.Sprint("Hi,%s\n",name)
-	return fmt.Sprintf("Hi,%s!\n",name)
+func Hi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi,%s!", name), nil
+	case "pt":
+		return fmt.Sprintf("0i,%s!", name), nil
+	case "es":
+		return fmt.Sprintf("iHola,%s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour,%s", name), nil
+	default:
+		return "", errors.New("unknown language")
+	}
+	//fmt.Sprint("Hi,%s\n",name)
+	//return fmt.Sprintf("Hi,%s!\n",name)
 }
